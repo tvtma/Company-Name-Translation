a20ca72c1dd5c35868bcb61c96aa547f1eeb4c52
leotran <leo.tran@ma.tvtmarine.com>
2015-01-08 11:48:29 +0700

    Set translatable is true for company name.

---
+++ __openerp__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    @package t_to_website_filter TO Compnay Name Translation for Odoo 8.0
+#    @copyright Copyright (C) 2015 T.V.T Marine Automation (aka TVTMA). All rights reserved.#
+#    @license http://www.gnu.org/licenses GNU Affero General Public License version 3 or later; see LICENSE.txt
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name' : 'TO Company Name Translation',
+    'version': '1.0.0',
+    'author' : 'T.V.T Marine Automation (aka TVTMA)',
+    'website': 'http://ma.tvtmarine.com',
+    'summary': 'Translate Company Name',
+    'sequence': 30,
+    'category': 'Hidden',
+    'description':"""
+Company Name Translation
+========================
+This module allow user translate company name
+    """,
+    'depends': ['base'],
+    'data': [
+            
+    ],
+    'installable': True,
+    'application': False,
+}
---
+++ res_company.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    @package t_to_website_filter TO Partner Name Translation for Odoo 8.0
+#    @copyright Copyright (C) 2015 T.V.T Marine Automation (aka TVTMA). All rights reserved.#
+#    @license http://www.gnu.org/licenses GNU Affero General Public License version 3 or later; see LICENSE.txt
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp import fields,models
+    
+class res_company(models.Model):
+    _inherit = 'res.company'    
+    
+    name = fields.Char(string="Company Name", size=128, required=True, store=True, related='partner_id.name', translate=True)
